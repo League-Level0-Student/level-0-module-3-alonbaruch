9da813823cda2001038b5e33aa209cd63d249061
League Student <league@iMac-18.attlocal.net>
2019-10-19 11:28:36 -0700

    rf
    gf

---
+++ src/_04_are_you_happy/cee.java
@@ -0,0 +1,46 @@
+package _04_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class cee {public static void main(String[] args) {
+	
+ String a = JOptionPane.showInputDialog(null,"are you happy");
+   
+   if(a.equals("yes")) {
+	 JOptionPane.showMessageDialog(null, "keep doing what you are doing");
+   }
+ 		if(a.equals("no")) {
+ 			String b = JOptionPane.showInputDialog(null, "do you want to be happy");
+ 		
+
+if (b.equals("no")) {
+
+
+JOptionPane.showInputDialog(null, "Do whatever your doing");
+
+
+
+
+}
+if(b.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Change something");
+}
+ 		
+ 		
+ 		
+ 		}
+
+
+
+
+
+
+
+
+
+
+
+
+ 		}
+}
+
--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,18 +8,22 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
-		// 3. Play the sound that many times
+String d = JOptionPane.showInputDialog(null, "how many shiny object do you want");
+		int a = Integer.parseInt(d);
+// 3. Play the sound that many times
+for (int i = 0; i < a; i++) {
+	
 
 		// 1. Call the method below
-
+playMisterZee();
 	}
-
+	}
 	public static void playMisterZee() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(ShinyObjects.class.getResource("shiny-objects.wav"));
