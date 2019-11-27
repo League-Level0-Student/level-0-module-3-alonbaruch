299ded5f3df9a12bcc4212a888ab3c35fa0dff08
League Student <league@iMac-37.attlocal.net>
2019-11-26 19:34:35 -0800

    asvXCV
    VDDVSB

--- src/_16_obedient_robot/g.java
+++ src/_16_obedient_robot/g.java
@@ -6,49 +6,71 @@ import javax.swing.JOptionPane;
 
 import org.jointheleague.graphical.robot.Robot;
 
-public class g {	public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	String a =JOptionPane.showInputDialog(null,"what color do you want the shape to be");
-	Robot r = new Robot();
-	r.setSpeed(5);
-	r.penDown();
-if(a.equals("black")) {
-	
-	r.setPenColor(Color.BLACK);
-}
-if(a.equals("white")) {
-	
-	r.setPenColor(Color.white);
-}
-if(a.equals("gray")) {
-	
-	r.setPenColor(Color.gray);
-}
-for (int i = 0; i < 4; i++) {
-	r.move(200);
-	r.turn(90);
-	
-}
-}
+public class g {
+	static Robot r = new Robot();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = JOptionPane.showInputDialog(null, "what color do you want the shape to be");
+		String b = JOptionPane.showInputDialog(null,"what shape do you want");
+		r.penDown();
+		if (a.equals("black")) {
 
+			r.setPenColor(Color.black);
+		}
+		if (a.equals("white")) {
+
+			r.setPenColor(Color.white);
+		}
+		if (a.equals("gray")) {
+
+			r.setPenColor(Color.gray);
+		}
+		if(b.equals("square")) {
+			drawSquare();
+		}
+		if(b.equals("triangle")) {
+			drawTriangle();
+		}
+		if(b.equals("circle")) {
+			drawCircle();
+		}
+		
+		
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
 
 
+	}
+	
+	static void drawSquare() {
+		for (int i = 0; i < 4; i++) {
+			r.penDown();
+			r.move(200);
+			r.turn(90);
+	}
+	}
+	static void drawTriangle() {
+r.penDown();
+		r.move(200);
+r.turn(140);
+r.move(200);
+r.turn(140);
+r.move(200);
+	}
+static void drawCircle() {
+	for (int i = 0; i < 200; i++) {
+		r.penDown();
+	
+	r.turn(2);
+	r.move(3);
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+}
